30463c5bd182e91ef02f6e49f74db2b13e8c6df7
alec aivazis <alec@aivazis.com>
2017-01-30 01:21:36 -0800

    added plus method

--- index.js
+++ index.js
@@ -4,10 +4,43 @@ import { Map } from 'extendable-immutable'
 
 class Matrix extends Map {
 
-    constructor({a=0, b=0, c=0, d=0, e=0, f=0}={}) {
+    constructor({a=1, b=0, c=0, d=1, e=0, f=0}={}) {
         // save those instance values in an immutable map internally
         return super({a, b, c, d, e, f})
     }
+
+    plus(other) {
+        // grab the elements from the other matrix
+        const {a, b, c, d, e, f} = other
+
+        let out = [[], [], []],
+            m = [[this.a, this.c, this.e], [this.b, this.d, this.f], [0, 0, 1]],
+            matrix = [[a, c, e], [b, d, f], [0, 0, 1]],
+            x, y, z, res
+
+        for (x = 0; x < 3; x++) {
+            for (y = 0; y < 3; y++) {
+                res = 0;
+                for (z = 0; z < 3; z++) {
+                    res += m[x][z] * matrix[z][y];
+                }
+                out[x][y] = res;
+            }
+        }
+
+        // batch the mutations together
+        return this.withMutations(map => {
+            // add ea
+            map.set('a', out[0][0])
+               .set('b', out[1][0])
+               .set('c', out[0][1])
+               .set('d', out[1][1])
+               .set('e', out[0][2])
+               .set('f', out[1][2])
+        })
+    }
+
+    // element accessors
 
     get a() {
         return this.get('a')
--- test.js
+++ test.js
@@ -8,10 +8,10 @@ test('Matrix can be created with no args', t => {
     const mat = new Matrix()
     // make sure that all of the
     t.deepEqual(mat.toJS(), {
-        a: 0,
+        a: 1,
         b: 0,
         c: 0,
-        d: 0,
+        d: 1,
         e: 0,
         f: 0,
     })
@@ -28,7 +28,41 @@ test('Can retrieve individual elements', t => {
     t.is(mat.a, 1)
     t.is(mat.b, 2)
     t.is(mat.c, 0)
-    t.is(mat.d, 0)
+    t.is(mat.d, 1)
     t.is(mat.e, 0)
     t.is(mat.f, 0)
+})
+
+test('Can add two matrices together', t => {
+    // create two matrices to add
+    const mat1 = new Matrix({a: 1, b: 0, c: 0, d: 1, e: 5, f: 5})
+    const mat2 = new Matrix({a: 1, b: 0, c: 0, d: 1, e: 10, f: 10})
+
+    // perform the sum
+    const sum = mat1.plus(mat2)
+
+    // make sure the result matches expectation
+    t.deepEqual(sum.toJS(), {
+        a: 1,
+        b: 0,
+        c: 0,
+        d: 1,
+        e: 15,
+        f: 15,
+    })
+
+    // add two 90 degree rotations
+    const mat3 = new Matrix({a: 0, b: 1, c: -1, d:0, e:0, f:0})
+    const mat4 = new Matrix({a: 0, b: 1, c: -1, d:0, e:0, f:0})
+    const rotSum = mat3.plus(mat4)
+
+    // make sure it matches expectations
+    t.deepEqual(rotSum.toJS(), {
+        a: -1,
+        b: 0,
+        c: 0,
+        d: -1,
+        e: 0,
+        f: 0
+    })
 })
