c58c5add0bfdf883a10c9db420b6c96c7c38bb29
alec aivazis <alec@aivazis.com>
2017-01-30 19:24:39 -0800

    fixed bug preventing matrix frmo being compiled

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,21 @@
+var path = require('path')
+
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'build.js',
         library: 'webpackNumbers',
         libraryTarget: 'umd',
+    },
+    module: {
+        rules: [
+            {
+                test: /.jsx?/,
+                include: [
+                    path.resolve(__dirname, "src")
+                ],
+                loader: "babel-loader"
+            }
+        ]
     }
 }
